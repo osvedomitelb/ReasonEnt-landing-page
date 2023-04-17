4568b04dfe412528049955283dd1e2090faf45af
Warlord of Barbarians <anton.zakrasnyanyy@gmail.com>
2023-04-17 22:07:24 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+
+
+/**************************PopUp Section*****************************/
+
+let popUpSection = document.querySelector("#popUpSection");
+let closeBtn = document.querySelector("#closeBtn");
+let sliderSection = document.querySelector("#sliderSection");
+
+closeBtn.addEventListener("click", function() {
+    popUpSection.style.display = "none";
+});
+/**************************Slider Section*****************************/
+
+
+    let slideIndex = 1;
+    showDivs(slideIndex);
+
+    function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+
+    function showDivs(n) {
+    let i;
+    let x = document.getElementsByClassName("mySlides");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length}
+    for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+}
+    x[slideIndex-1].style.display = "block";
+}
